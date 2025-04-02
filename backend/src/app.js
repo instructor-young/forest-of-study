b0ec93558baf125f99d9e621bea23d065bfd6791
Yoo JinYoung <young@acadeem.io>
2025-04-02 15:20:47 +0900

    [backend] feat: errorHandler 추가

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
 const router = require("./modules/index.module");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const PORT = 5555;
@@ -9,6 +9,7 @@ const PORT = 5555;
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server started to listen at port ${PORT}...`);
--- backend/src/utils/functions.utils.js
+++ backend/src/utils/functions.utils.js
@@ -2,10 +2,13 @@ const prisma = require("../db/prisma/client.prisma");
 const bcrypt = require("bcrypt");
 
 async function throwErrorIfStudyPasswordIsNotCorrect(studyId, password) {
-  const { encryptedPassword } = await prisma.study.findUnique({
+  const study = await prisma.study.findUnique({
     where: { id: studyId },
     select: { encryptedPassword: true },
   });
+  if (!study) throw new Error(401);
+
+  const { encryptedPassword } = study;
   const isCorrect = await bcrypt.compare(password, encryptedPassword);
 
   if (!isCorrect) throw new Error(401);
